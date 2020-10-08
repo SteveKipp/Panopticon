993f32d81c769b50e427fc7e4e86b9cc6ccf7f4a
Steven Kipp <sk1pp@protonmail.com>
2020-10-07 22:03:17 -0500

    added connection logging with showing peer_addr

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod tcp;
 
 fn main() {
     println!("Starting server on port 22...");
-    tcp::listen(22);
+    tcp::listen(7878);
 }
--- src/tcp.rs
+++ src/tcp.rs
@@ -1,13 +1,25 @@
 use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
 
 pub fn listen(port: i32) {
-    let addr = format!("127.0.0.1:{}", port);
+    let addr = format!("0.0.0.0:{}", port);
 
     let listener = TcpListener::bind(addr).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection Attempt!")
+        println!("----Incoming Connection----");
+        connection_details(stream);
     }
+}
+
+fn connection_details(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+    println!("Peer: {} \n Details: {}", stream.peer_addr().unwrap(),
+                                        String::from_utf8_lossy(&buffer[..]));
+
 }
