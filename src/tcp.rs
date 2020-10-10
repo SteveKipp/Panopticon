035cc7c67283838dab7419d1fe3466273611ab51
Steven Kipp <steven.a.kipp.ctr@nga.mil>
2020-10-10 16:01:00 -0500

    added address lookup via ipinfo with env var for key

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,6 +1,9 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::env;
+
+use ipinfo::{IpInfo, IpInfoConfig};
 
 pub fn listen(port: i32) {
     let addr = format!("0.0.0.0:{}", port);
@@ -19,7 +22,23 @@ fn connection_details(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    println!("Peer: {} \n Details: {}", stream.peer_addr().unwrap(),
-                                        String::from_utf8_lossy(&buffer[..]));
+    let peer = stream.peer_addr().unwrap();
 
+    println!("Peer: {} \n Request: {}", peer, String::from_utf8_lossy(&buffer[..]));
+
+    //stop note:  how to conver SocketAddr to &str
+    addr_lookup(peer.to_str());
+}
+
+fn addr_lookup(addr: &str) {
+    let key = env::var("IPINFO_KEY").unwrap();
+    let config = IpInfoConfig { token: Some(key.to_string()), ..Default::default() };
+    let mut ipinfo = IpInfo::new(config).expect("should construct");
+    let res = ipinfo.lookup(&[addr]);
+
+    println!("---- Peer Address Lookup ---");
+    match res {
+        Ok(r) => println!("{}: {}", addr, r[addr].hostname),
+        Err(e) => println!("error occurred: {}", &e.to_string()),
+    }
 }
