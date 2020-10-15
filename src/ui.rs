491f3642ecc74096e751b105d7219167066ec763
Steven Kipp <steven.a.kipp.ctr@nga.mil>
2020-10-15 17:27:08 -0500

    UI tweaks, something closer to not a turd

--- src/ui.rs
+++ src/ui.rs
@@ -5,13 +5,15 @@ use druid::{
     AppLauncher, Color, Data, Lens, UnitPoint, Widget, WidgetExt, WindowDesc,
 };
 
+
+//I think this is the state of a component
 #[derive(Clone, Data, Lens)]
 struct AppData {
     incoming: Vector<u32>,
 }
 
 pub fn main() {
-    let main_window = WindowDesc::new(ui_builder);
+    let main_window = WindowDesc::new(ui_builder).window_size((800.0, 600.0));
     let inc = vector![0, 1, 2, 3];
     let data = AppData { incoming: inc };
     // this listen has to be run asynchronously from the ui
@@ -25,22 +27,23 @@ pub fn main() {
 
 fn ui_builder() -> impl Widget<AppData> {
     let log = Scroll::new(List::new(|| {
-            Label::new(|item: &u32, _env: &_| format!("Incoming -> #{}", item))
-                .align_vertical(UnitPoint::LEFT)
-                .padding(10.0)
-                .expand()
-                .height(50.0)
-                .background(Color::rgb(0.5, 0.5, 0.5))
-        })).lens(AppData::incoming);
+                Label::new(|item: &u32, _env: &_| format!("Incoming -> #{}", item))
+                    .align_vertical(UnitPoint::RIGHT)
+                    .expand()
+                    .height(50.0)
+                    .width(50.0)
+                    .background(Color::rgb(0.5, 0.5, 0.5))
+                })).vertical().lens(AppData::incoming);
 
     Flex::row()
         .with_flex_child(
-            Flex::column().with_flex_child(
+            Flex::column()
+                .with_flex_child(
                 Label::new("Map Widget Here")
-                    .padding(100.0),
-                1.0),
-        1.0)
+                    .center(),
+                1.0).fix_width(500.0),
+            1.0)
         .with_flex_child(
-            Flex::column().with_flex_child(log, 1.0),
-        1.0)
+            Flex::column()
+                .with_flex_child(log, 1.0), 1.0)
 }
