6f2c2e5ae370e4f04c21446ef0155c52e407d2d5
Steve Kipp <sk1pp@protonmail.com>
2020-11-17 23:19:38 -0600

    added constructor to grid struct

--- src/grid.rs
+++ src/grid.rs
@@ -10,5 +10,17 @@ pub struct Grid {
 // too - for for creating points, and the applying the canvas widget to 
 // the grid struct -- good ol' IMPL
 impl Grid {
+    const MAX_LAT: f32 = 90.0;
+    const MIN_LAT: f32 = -90.0;
 
+    const MAX_LONG: f32 = 180.0;
+    const MIN_LONG: f32 = -180.0;
+
+    pub fn build() -> Self{
+        //here Self = Grid
+        Self {
+            scaling: 0.01,
+            translation: Vector::default(),
+        }
+    }
 }
