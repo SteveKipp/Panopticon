04010e4fa26ac95d6343376924da370828c1389b
Steve Kipp <sk1pp@protonmail.com>
2020-11-18 23:11:26 -0600

    added structure for applying canvas to grid

--- src/grid.rs
+++ src/grid.rs
@@ -1,14 +1,21 @@
-use iced::{ Vector, };
+use iced::{         
+    canvas::{
+        self, Cache, Canvas, Cursor, Event, Frame, Geometry, Path, Text,
+    },
+    Vector, Element, Length, Rectangle,
+};
 
 pub struct Grid {
     scaling: f32,
-    translation: Vector
+    translation: Vector,
+    show_lines: bool,
 }
 
-//STOPNOTE - implement the maxs, min for lat long
-// looks like there are some functions that can be implemented here
-// too - for for creating points, and the applying the canvas widget to 
-// the grid struct -- good ol' IMPL
+#[derive(Debug, Clone)]
+pub enum Message {
+    Pinged
+}
+
 impl Grid {
     const MAX_LAT: f32 = 90.0;
     const MIN_LAT: f32 = -90.0;
@@ -21,6 +28,36 @@ impl Grid {
         Self {
             scaling: 0.01,
             translation: Vector::default(),
+            show_lines: false,
         }
+    }
+
+    pub fn update(&mut self, message: Message) {
+        match message{
+            Message::Pinged => {
+                println!("Incoming connection passed to lat long grid")
+            }
+        }
+    }
+
+    pub fn view<'a>(&'a mut self) -> Element<'a, Message> {
+        Canvas::new(self)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .into()
+    }
+
+}
+
+//STOPNOTE - 
+//this is going to be a beast of a function 
+// this impl is required in order for the view to function to work
+impl<'a> canvas::Program<Message> for Grid {
+    fn update(&mut self, event: Event, bounds: Rectangle, cursor: Cursor,) -> Option<Message> {
+        Some(Message::Pinged)
+    }
+
+    fn draw(&self, bounds: Rectangle, cursor: Cursor)  -> Vec<Geometry> {
+        vec![]
     }
 }
